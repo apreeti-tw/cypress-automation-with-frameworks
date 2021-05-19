f04534b6d125c1a3c1ce13661fdd5c3c000684d9
apreeti-tw <apreeti@thoughtworks.com>
2021-05-20 01:07:44 +0530

    Validate attributes and behavior in the test

--- cypress/integration/tests/DemoTest.js
+++ cypress/integration/tests/DemoTest.js
@@ -9,5 +9,9 @@ describe('Demo test to start frameworks', function (){
         cy.visit('https://rahulshettyacademy.com/angularpractice/')
         cy.contains('Name').next().type(this.data.name)
         cy.get('#exampleFormControlSelect1').select(this.data.gender)
+
+        cy.contains('Two-way Data Binding example').children('input').should('have.value', this.data.name)
+        cy.contains('Name').next().should('have.attr', 'minlength', '2')
+        cy.get('#inlineRadio3').should('be.disabled')
     })
 })
