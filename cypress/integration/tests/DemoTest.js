76958f7e1e23b8b08992e47854cd486722b864c6
apreeti-tw <apreeti@thoughtworks.com>
2021-05-20 00:45:38 +0530

    Read data from fixtures

--- cypress/integration/tests/DemoTest.js
+++ cypress/integration/tests/DemoTest.js
@@ -1,10 +1,13 @@
 describe('Demo test to start frameworks', function (){
     before(function (){
-        cy.visit('https://rahulshettyacademy.com/angularpractice/   ')
+        cy.fixture('example').then(function (data){
+            this.data = data
+        })
     })
 
     it('should be able to submit the form', function () {
-        cy.contains('Name').next().type('Bob')
-        cy.get('#exampleFormControlSelect1').select('Female')
+        cy.visit('https://rahulshettyacademy.com/angularpractice/')
+        cy.contains('Name').next().type(this.data.name)
+        cy.get('#exampleFormControlSelect1').select(this.data.gender)
     })
 })
