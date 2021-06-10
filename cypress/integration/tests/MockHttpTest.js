52ade28d841dd1e3a0c16a096c0679b245c8475d
apreeti-tw <apreeti@thoughtworks.com>
2021-06-10 22:54:03 +0530

    Add test to intercept http request and mock http response

---
+++ cypress/integration/tests/MockHttpTest.js
@@ -0,0 +1,14 @@
+describe('Mock request/response tests', function (){
+    it('should mock http response to get results', function () {
+        cy.intercept(
+            'GET',
+            '/Library/GetBook.php?AuthorName=shetty', {
+                statusCode: 200,
+                body:[{"book_name":"RestAssured with Java","isbn":"RSU","aisle":"2301"}]
+            }).as('getBooks')
+        cy.visit('/angularAppdemo')
+        cy.get('.btn-primary').click()
+        cy.wait('@getBooks')
+        cy.get('p').should('have.text', 'Sorry we have only one book available')
+    });
+})
